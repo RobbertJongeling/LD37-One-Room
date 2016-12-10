4a5c88f37e4102729d9614ee84365a8a9714d94b
RobbertJongeling <r.m.jongeling@student.tue.nl>
2016-12-10 14:50:01 +0100

    fixe

--- main.lua
+++ main.lua
@@ -5,8 +5,6 @@ require "menu"
 function love.load()
   if arg and arg[#arg] == "-debug" then require ("modedebug").start() end
   gameStarted = false
-  wheight = love.window.fromPixels(love.graphics.getHeight())
-  wwidth = love.window.fromPixels(love.graphics.getWidth())
 end
 
 function love.draw()
