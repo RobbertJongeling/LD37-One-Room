287c0f856dd28e4dc86286dfd2ef04eb0e8c5ba9
RobbertJongeling <r.m.jongeling@student.tue.nl>
2016-12-10 22:43:37 +0100

    drawing airports + sweep asset

--- airport.lua
+++ airport.lua
@@ -8,13 +8,13 @@ function generate_airport()
   runway1.x1 = 0
   runway1.x2 = 1
   runway1.y1 = love.math.random()
-  runway1.y1 = love.math.random()
+  runway1.y2 = love.math.random()
   airport.runways[0] = runway1
 
   runway2.x1 = love.math.random()
   runway2.x2 = love.math.random()
   runway2.y1 = 0
-  runway2.y1 = 1
+  runway2.y2 = 1
   airport.runways[1] = runway2
 
   airport.planes = generate_planes(3)
--- main.lua
+++ main.lua
@@ -21,6 +21,8 @@ function love.load()
   screenwidth = 1700
   screenheight = 650
 
+  sf = 0.00005
+
   airports = {}
   for i = 1, 4 do
     airports[i] = generate_airport()
@@ -89,22 +91,36 @@ function draw_radar(panel, airport)
   local a = bordered_panel.x  + (bordered_panel.width / 2)
   local b = bordered_panel.y + (bordered_panel.height / 2)
 
+  love.graphics.setLineWidth(1)
   love.graphics.circle("line", a, b, smallest / 2)
 
   draw_plane(bordered_panel, airport.planes)
+  draw_runways(bordered_panel, airport.runways)
+end
+
+function draw_runways(panel, runways)
+  for i, r in pairs(runways) do
+    local s = sf * panel.width
+
+    love.graphics.setLineWidth(2)
+    love.graphics.setColor(radar_green)
+    love.graphics.line((r.x1 * panel.height + panel.x + ((panel.width - panel.height) / 2)),
+                        (r.y1 * panel.height + panel.y),
+                        (r.x2 * panel.height + panel.x + ((panel.width - panel.height) / 2)),
+                        (r.y2 * panel.height + panel.y))
+  end
 end
 
 function draw_plane(panel, planes)
   for i, p in pairs(planes) do
 
-    local sx = .00005 * panel.width
-    local sy = .00005 * panel.width
+    local s = sf * panel.width
     local ox = 0
     local oy = 0
     local drawx = p.x * panel.height + panel.x + ((panel.width - panel.height) / 2)
     local drawy = p.y * panel.height + panel.y
     if drawx > panel.x and drawx < (panel.x + panel.width) and drawy > panel.y and drawy < (panel.y + panel.height) then
-      love.graphics.draw(plane_asset, drawx, drawy, p.rot, sx, sy, ox, oy)
+      love.graphics.draw(plane_asset, drawx, drawy, p.rot, s, s, ox, oy)
     end
   end
 end
