c41b4e40db4a69c157976a6ebbaffd0d32a6e072
David Vollmar <david@vollmar.nl>
2016-12-10 16:24:13 +0100

    ha ik was eerst

--- main.lua
+++ main.lua
@@ -4,7 +4,9 @@ require "menu"
 
 function love.load()
   if arg and arg[#arg] == "-debug" then require ("modedebug").start() end
-  radar_green = {2, 206, 63}
+  radar_green = {2, 206, 63 }
+  load_assets()
+  gameStarted = true
 end
 
 
@@ -20,7 +22,18 @@ function draw_radar(x, y, width, height)
   local b = y + (height / 2)
 
   love.graphics.circle("line", a, b, width / 2)
-  gameStarted = false
+  draw_plane(x, y, math.pi * .25)
+  draw_plane(x + width, y + height, math.pi * 1.25)
+  draw_plane(x, y + height, math.pi * 1.75)
+  draw_plane(x + width, y, math.pi * .75)
+end
+
+function draw_plane(x, y, rot)
+  sx = .1
+  sy = .1
+  ox = 0
+  oy = 0
+  love.graphics.draw(plane_asset, x, y, rot, sx, sy, ox, oy)
 end
 
 function love.draw()
@@ -31,5 +44,9 @@ function love.draw()
 end
 
 function drawGame()
-  draw_radar(120, 120, 100, 100)
+  draw_radar(120, 120, 500, 500)
+end
+
+function load_assets()
+  plane_asset = love.graphics.newImage("assets/black-plane.png")
 end
