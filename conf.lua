7e3ac45d93fdbcf9d9e278ac1c0bbc580be058ea
RobbertJongeling <r.m.jongeling@student.tue.nl>
2016-12-10 14:41:58 +0100

    nu wel

--- conf.lua
+++ conf.lua
@@ -1,8 +1,6 @@
 function love.conf(c)
   c.title = "Game for Ludum Dare 37 - Theme: One Room"
   local window = c.screen or c.window
-  window.width = 1920
-  window.height = 1080
   window.fullscreen = true
   window.highdpi = true
 end
